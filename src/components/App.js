3a4ad0a5bc00d7fe5efb30e10298eaee9e721f07
svc_relojctrl <svc_relojctrl@achs.cl>
2020-03-17 12:25:43 -0300

    dd App Component refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
+import ConnectedTodos from "../components/Todos";
+import ConnectedGoals from "../components/Goals";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(handleInitialData());
+  }
+  render() {
+    if (this.props.loading === true) return <h3>Loading</h3>;
+
+    return (
+      <div>
+        <ConnectedTodos />
+        <ConnectedGoals />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(state => ({ loading: state.loading }))(App);
