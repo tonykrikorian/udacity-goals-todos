67da4b9e2c16d1ec8d8f2fe78919cd13db05ba33
svc_relojctrl <svc_relojctrl@achs.cl>
2020-03-17 11:16:17 -0300

    Apply index middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,1 +1,7 @@
+import checker from "../middleware/checker";
+import greatGoal from "../middleware/greatGoal";
+import logger from "../middleware/logger";
+import { applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
+export default applyMiddleware(thunk, checker, greatGoal, logger);
